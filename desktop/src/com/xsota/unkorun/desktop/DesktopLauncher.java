c23c4c8bdd8928a83fa826ae05036c0a864e3146
sota <git@xsota.com>
2016-11-26 17:22:30 +0900

    init

---
+++ desktop/src/com/xsota/unkorun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.xsota.unkorun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.xsota.unkorun.UnkoRun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UnkoRun(), config);
+	}
+}
