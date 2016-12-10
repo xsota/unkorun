a9ab12a6cc97f8d3059505fd0b74541d9b0cbc15
sota <git@xsota.com>
2016-12-10 19:53:15 +0900

    click image to jump

--- core/src/com/xsota/unkorun/GameScreen.kt
+++ core/src/com/xsota/unkorun/GameScreen.kt
@@ -3,7 +3,13 @@ package com.xsota.unkorun
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.math.Interpolation
+import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.badlogic.gdx.utils.viewport.FillViewport
 
 
@@ -13,9 +19,30 @@ import com.badlogic.gdx.utils.viewport.FillViewport
 
 class GameScreen(game: UnkoRun) : Screen {
   val stage : Stage = Stage(FillViewport(game.SCREEN_SIZE_WIDTH, game.SCREEN_SIZE_HEIGHT))
+  val jumpUnkoImage: Image = Image(Texture("badlogic.jpg"))
 
   override fun show() {
     Gdx.input.inputProcessor = stage
+    jumpUnkoImage.setPosition(100f,0f)
+
+    jumpUnkoImage.addListener(object : ClickListener(){
+      override fun clicked(event: InputEvent?, x: Float, y: Float) {
+        if (jumpUnkoImage.hasActions()){
+          return
+        }
+
+        val up = Actions.moveBy(0f, 120f, 0.3f, Interpolation.pow2)
+        val down = Actions.moveBy(0f, -120f, 0.5f, Interpolation.pow2)
+
+        val jump = Actions.sequence()
+        jump.addAction(up)
+        jump.addAction(down)
+
+        jumpUnkoImage.addAction(jump)
+      }
+    } )
+
+    stage.addActor(jumpUnkoImage)
   }
 
   override fun render(delta: Float) {
